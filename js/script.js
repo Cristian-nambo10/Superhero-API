a33aafb09c0555d168e4e6439b9707fb3b8d9094
Cristian Nambo <nambocristian10@gmail.com>
2022-05-10 14:16:32 -0500

    WIIP: Added new functions and got image api working

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@
 
     // const url = `https://www.superheroapi.com/api.php/338148107599656/${logic}/powerstats`;
 
-
+    
 
 // Make my jQuery variables
 let $name = $('#name');
@@ -14,21 +14,22 @@ let $strength = $('.strength');
 let $speed = $('.speed');           // Made my variables using classes
 let $power = $('.power');
 let $combat = $('.combat');
+let $image = $('.image')
 let $form = $('form');
 let $input = $('input[type="text"]')
 
 // Now I need event listener ...
 $form.on('submit', handleGetData)
 
-
+let userInput = $input.val();
 // A function for my stats
 
 function handleGetData(e) {
     e.preventDefault()  
-    const userInput = $input.val();
-    const logic = document.getElementById('text-box').value;
-    const url = `https://www.superheroapi.com/api.php/338148107599656/${logic}/powerstats`;
-
+    userInput = $input.val();
+    // const logic = document.getElementById('text-box').value;
+    const url = `https://www.superheroapi.com/api.php/338148107599656/${userInput}/powerstats`;
+    // const imgUrl = `https://www.superheroapi.com/api.php/338148107599656/${userInput}/image`;
 
     $.ajax(url).then(function(data) {
         // console.log('data is ready!')
@@ -39,6 +40,28 @@ function handleGetData(e) {
         $speed.text(data.speed);
         $power.text(data.power);
         $combat.text(data.combat);
+        
+        // $('main').append(`<img src="${data.url}"/>`)
     }) 
+}
+
+
+$form.on('submit', imageData)
+
+function imageData(e) {
+    e.preventDefault();
+    userInput = $input.val();
+    const imgUrl = `https://www.superheroapi.com/api.php/338148107599656/${userInput}/image`;
+    if (userInput === true) {
+        return userInput.empty();
+    }
+
+    $.ajax(imgUrl).then(function(data) {
+        console.log('data is ready!')
+        console.log(data)
+
+        $('main').append(`<img src="${data.url}"/>`)
+    })
+
 }
 
