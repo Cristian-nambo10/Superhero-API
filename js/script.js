03b9d96eaeeb4a937d9f7b8036e379dbf2416243
Cristian Nambo <nambocristian10@gmail.com>
2022-05-11 11:09:12 -0500

    WIP: Added new styles to DOM and new tag for next section of API

--- js/script.js
+++ js/script.js
@@ -17,35 +17,43 @@ let $combat = $('.combat');
 let $image = $('.image')
 let $form = $('form');
 let $input = $('input[type="text"]')
+let $article = $('.appearance')
 
 // Stylling for DOM elements ....
 const mainName = document.querySelector('#name')
 mainName.style.color = '#FDE311';
 mainName.style.fontSize = '26px';
+mainName.style.fontFamily = 'helvetica';
 
 const strength = document.querySelector('.strength')
 strength.style.color = '#FDE311';
 strength.style.fontSize = '26px';
+strength.style.fontFamily = 'helvetica';
+
 
 const speed = document.querySelector('.speed')
 speed.style.color = '#FDE311';
 speed.style.fontSize = '26px';
+speed.style.fontFamily = 'helvetica';
+
 
 const power = document.querySelector('.power')
 power.style.color = '#FDE311';
 power.style.fontSize = '26px';
+power.style.fontFamily = 'helvetica';
 
 const combat = document.querySelector('.combat')
 combat.style.color = '#FDE311';
 combat.style.fontSize = '26px';
+combat.style.fontFamily = 'helvetica';
 
 // Now I need event listener ...
-$form.on('submit', handleGetData)
+$form.on('submit', statsData)
 
 let userInput = $input.val();
 // A function for my stats
 
-function handleGetData(e) {
+function statsData(e) {
     e.preventDefault()  
     userInput = $input.val();
     // const logic = document.getElementById('text-box').value;
@@ -82,6 +90,18 @@ function imageData(e) {
         console.log(data)
 
         $('main').append(`<img src="${data.url}"/>`)
+    })
+
+}
+
+
+function getLooks(e) {
+    e.preventDefault();
+    userInput = $input.val();
+    const app = `https://www.superheroapi.com/api.php/338148107599656/${userInput}/appearance`;
+
+    $.ajax(app).then(function(data) {
+        
     })
 
 }
